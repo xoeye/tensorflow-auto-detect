0c474aa11dcbdd8a3c233099447dc8956076a58a
Matt Eby <meby@xoi.io>
2019-10-09 15:06:20 -0500

    tensorflow auto detect for 1.13.1

---
+++ setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+from setuptools import find_packages
+from subprocess import Popen
+from subprocess import PIPE
+
+
+VERSION = 1.13.1
+
+def has_gpu():
+    try:
+        p = Popen(["nvidia-smi"], stdout=PIPE)
+        stdout, stderror = p.communicate()
+        return True
+    except Exception:
+        return False
+
+
+if has_gpu():
+    install_requires.append(f"tensorflow-gpu=={VERSION}")
+else:
+    install_requires.append(f"tensorflow=={VERSION}")
+
+
+setup(
+    name="tensorflow-auto-detect",
+    version="1.0",
+    author="Matt Eby",
+    description="Tensorflow package to auto detect and install tensorflow or tensorflow-gpu based on CUDA availability.",
+    url="https://github.com/xoeye/tensorflow-auto-detect",
+    classifiers=(
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=install_requires
+)
